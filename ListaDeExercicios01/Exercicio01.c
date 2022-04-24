e6c38eaefdfe3a88dcaf9c7c0d66f85543aea430
Gabriel L. C <102259679+GabrielLBl@users.noreply.github.com>
2022-04-24 20:43:23 -0300

    Update Exercicio01.c

--- ListaDeExercicios01/Exercicio01.c
+++ ListaDeExercicios01/Exercicio01.c
@@ -7,21 +7,21 @@ int main(void)
     vDia = 1;
     vMes = 1;
     vAno = 1;
-    
+
     while(vDia > 0 && vMes > 0 && vAno > 0)
     {
-    	printf("Informe uma dia: ");
+    	printf("\nInforme uma dia: ");
     	scanf("\n %d", &vDia);
     	printf("Informe uma mes: ");
     	scanf("\n %d", &vMes);
     	printf("Informe uma ano: ");
     	scanf("\n %d", &vAno);
-    	
+
     	M = vMes;
     	D = vDia;
     	A = vAno;
     	S = vAno / 100;
-    	
+
     	if (M < 3){
     	    M = M + 10;
     	    A = vAno - 1;
@@ -29,31 +29,37 @@ int main(void)
     	else
     	    M = vMes - 2;
         A = A % 100;
-    	    
+
     	vDiaSemana = (((int)(2.6 * M - 0.1)) + D + A + (A/4) + (S/4) - 2 * S)%7;
     	if (vDiaSemana < 0)
     	    vDiaSemana = vDiaSemana + 7;
-    	    
+
+        if (vDia == 0 && vMes == 0 && vAno == 0)
+        {
+            printf("Programa finalizado.");
+            break;
+        }
+
     	if (vDiaSemana == 0)
-    	    printf("%d %d %d: Domingo", vDia, vMes, vAno);
+    	    printf("%d/%d/%d: Domingo\n", vDia, vMes, vAno);
     	else
     	    if (vDiaSemana == 1)
-    	        printf("%d %d %d: Segunda-feira", vDia, vMes, vAno);
+    	        printf("%d/%d/%d: Segunda-feira\n", vDia, vMes, vAno);
     	        else
     	            if (vDiaSemana == 2)
-    	                printf("%d/%d/%d: Terça-feira", vDia, vMes, vAno);
+    	                printf("%d/%d/%d: Terça-feira\n", vDia, vMes, vAno);
     	                else
                     	    if (vDiaSemana == 3)
-                    	        printf("%d %d %d: Quarta-feira", vDia, vMes, vAno);
+                    	        printf("%d/%d/%d: Quarta-feira\n", vDia, vMes, vAno);
                     	        else
                             	    if (vDiaSemana == 4)
-                            	        printf("%d %d %d: Quinta-feira", vDia, vMes, vAno);
+                            	        printf("%d/%d/%d: Quinta-feira\n", vDia, vMes, vAno);
                             	        else
                                     	    if (vDiaSemana == 5)
-                                    	        printf("%d %d %d: Sexta-Feira", vDia, vMes, vAno);
+                                    	        printf("%d/%d/%d: Sexta-Feira\n", vDia, vMes, vAno);
                                     	        else
                                             	    if (vDiaSemana == 6)
-                                            	        printf("%d %d %d: Sabado", vDia, vMes, vAno);
-        return 0;
+                                            	        printf("%d/%d/%d: Sabado\n", vDia, vMes, vAno);
     }
+    return 0;
 }
